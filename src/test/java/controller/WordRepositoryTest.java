5f40928b8394f2a4cd131d90d50f6dd24444662a
steigersdorfermarah <madschi.steigersdorfer@gmx.at>
2020-03-02 14:51:09 +0100

    added test_insertRecord

--- src/test/java/controller/WordRepositoryTest.java
+++ src/test/java/controller/WordRepositoryTest.java
@@ -16,10 +16,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class WordRepositoryTest {
 
-        String TABLE_NAME = "WORD";
-        WordRepository wordRepository = new WordRepository();
-        WordRepository repository;
-       DataSource dataSource = Database.getDataSource();
+    String TABLE_NAME = "WORD";
+    WordRepository repository;
+    DataSource dataSource = Database.getDataSource();
 
     @BeforeEach
     void beforeEach() {
@@ -48,45 +47,43 @@ class WordRepositoryTest {
        assertThat(repository).isNotNull();
    }
 
-        @Test
-        void test070_insertRecord() {
 
-            Word word1 = new Word("Katze", "cat");
+   @Test
+   void test030_deleteWord(){
 
-            WordRepository wordRepository = new WordRepository();
-            Word savedWord = wordRepository.save(word1);
+   }
 
-            Table wordTable = new Table(dataSource, TABLE_NAME);
-            output(wordTable).toConsole();
-            assertThat(wordTable).hasFieldOrProperty("Katze");
-//            org.assertj.db.api.Assertions.assertThat(wordTable).hasNumberOfRows(1);
-//            assertThat(word1).isEqualTo(savedWord);
-//            assertThat(savedWord.getEnglishWord()).isEqualTo("cat");
-        }
+   @Test
+   void test070_insertRecord() {
+       Word word1 = new Word("Katze", "cat");
+
+       repository.save(word1);
+
+       Table wordTable = new Table(dataSource, TABLE_NAME);
+       output(wordTable).toConsole();
+       assertThat(wordTable).isNotNull();
+
+   }
+   @Test
+   void test080_saveTwoWords() {
+       Word word01 = new Word("Hund", "dog");
+       Word word02 = new Word("Katze", "cat");
+
+       WordRepository personRepository = new WordRepository();
+       personRepository.createTable();
+       personRepository.save(word01);
+       personRepository.save(word02);
+
+       Table personTable = new Table(dataSource, TABLE_NAME);
+       output(personTable).toConsole();
+       org.assertj.db.api.Assertions.assertThat(personTable).hasNumberOfRows(2);
+   }
 
 
-        @Test
-        void test080_saveTwoWords() {
-            Word word01 = new Word("Hund", "dog");
-            Word word02 = new Word("Katze", "cat");
+   @Test
+   void test090_dropTable() {
+       repository.dropTable();
 
-            WordRepository personRepository = new WordRepository();
-            personRepository.createTable();
-            personRepository.save(word01);
-            personRepository.save(word02);
-
-            Table personTable = new Table(dataSource, TABLE_NAME);
-            output(personTable).toConsole();
-            org.assertj.db.api.Assertions.assertThat(personTable).hasNumberOfRows(2);
-        }
-
-
-        @Test
-        void test090_dropTable() {
-            repository.dropTable();
-
-            assertThat(repository.tableExists() == false);
-            //assertThat(repository).isNull();
-
-        }
-    }
+       assertThat(repository.tableExists() == false);
+   }
+}
