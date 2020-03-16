7ba2d35fd40de77d1fe8f323dae4a7001dffa249
isi <turner.isabel03@gmail.com>
2020-03-16 13:02:12 +0100

    need to fix WordRepositoryTest, name variables in WordController from fxml

--- src/main/java/view/WordController.java
+++ src/main/java/view/WordController.java
@@ -1,4 +1,23 @@
 package view;
 
-public class WordController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import model.Word;
+
+import javax.swing.table.TableColumn;
+import javax.swing.text.TableView;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class WordController implements Initializable {
+
+   // @FXML
+   // private TableView<Word> tableView;
+    // @FXML
+    //private TableColumn<Word, String> germanWordColumn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
