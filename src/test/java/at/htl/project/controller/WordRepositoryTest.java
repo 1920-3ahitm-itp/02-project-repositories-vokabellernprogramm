cddbc09bee801b862640dccb969c209f5eadafac
htl-leonding <t.stuetz@htl-leonding.ac.at>
2020-03-27 11:24:03 +0100

    add test

---
+++ src/test/java/at/htl/project/controller/WordRepositoryTest.java
@@ -0,0 +1,32 @@
+package at.htl.project.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WordRepositoryTest {
+
+  @Test
+  void save() {
+  }
+
+  @Test
+  void createTable() {
+  }
+
+  @Test
+  void delete() {
+  }
+
+  @Test
+  void dropTable() {
+  }
+
+  @Test
+  void tableExists() {
+  }
+
+  @Test
+  void getAllWords() {
+  }
+}
