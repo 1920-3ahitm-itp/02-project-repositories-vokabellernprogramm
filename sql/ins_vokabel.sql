ee185a6cedb2a507fdff9eea65d88fb50df3c84a
steigersdorfermarah <madschi.steigersdorfer@gmx.at>
2020-03-28 12:29:57 +0100

    added insert into event

--- sql/ins_vokabel.sql
+++ sql/ins_vokabel.sql
@@ -20,3 +20,9 @@ INSERT INTO eventtype (et_name) VALUES ('SCHULARBEIT');
 INSERT INTO eventtype (et_name) VALUES ('TEST');
 INSERT INTO eventtype (et_name) VALUES ('MITARBEITSKONTROLLE');
 INSERT INTO eventtype (et_name) VALUES ('UEBUNG');
+
+INSERT INTO event (e_et_id, e_w_id, e_date, e_descr)VALUES (1, 1, '2020-04-01 08:00:00', 'Schularbeit');
+INSERT INTO event (e_et_id, e_w_id, e_date, e_descr)VALUES (2, 2, '2020-05-13 10:00:00', 'Test');
+INSERT INTO event (e_et_id, e_w_id, e_date, e_descr)VALUES (3, 3, '2020-05-27 08:00:00', 'Mitarbeitskontrolle');
+INSERT INTO event (e_et_id, e_w_id, e_date, e_descr)VALUES (4, 4, '2020-6-02 12:45:00', 'Schularbeit');
+
