a40e1c2aae4072cfc686093e4f8718bcf60776e6
steigersdorfermarah <madschi.steigersdorfer@gmx.at>
2020-04-09 13:24:54 +0200

    added CategoryRepository + CategoryRepositoryTest - not implemented yet

---
+++ src/main/java/at/htl/project/controller/CategoryRepository.java
@@ -0,0 +1,28 @@
+package at.htl.project.controller;
+
+import at.htl.project.model.Category;
+
+import java.util.List;
+
+public class CategoryRepository implements Repository<Category> {
+
+    @Override
+    public void save(Category category) {
+
+    }
+
+    @Override
+    public void delete(long id) {
+
+    }
+
+    @Override
+    public List<Category> findAll() {
+        return null;
+    }
+
+    @Override
+    public Category findById(long id) {
+        return null;
+    }
+}
---
+++ src/test/java/at/htl/project/controller/CategoryRepositoryTest.java
@@ -0,0 +1,24 @@
+package at.htl.project.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryRepositoryTest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+}
