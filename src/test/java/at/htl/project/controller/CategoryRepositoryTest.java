5342672c69869733c9ae1fa4336987357db8ac90
steigersdorfermarah <madschi.steigersdorfer@gmx.at>
2020-04-20 15:23:26 +0200

    added tests

--- src/test/java/at/htl/project/controller/CategoryRepositoryTest.java
+++ src/test/java/at/htl/project/controller/CategoryRepositoryTest.java
@@ -5,6 +5,7 @@ import at.htl.project.model.Word;
 import org.assertj.db.api.Assertions;
 import org.assertj.db.type.Row;
 import org.assertj.db.type.Table;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import javax.sql.DataSource;
 
@@ -17,35 +18,75 @@ import static org.junit.jupiter.api.Assertions.*;
 class CategoryRepositoryTest {
 
     DataSource dataSource = Database.getDataSource();
-    CategoryRepository repository = new CategoryRepository();
+    static CategoryRepository repository;
+
+    @BeforeAll
+    static void beforeAll(){
+        repository = new CategoryRepository();
+    }
+
 
     @Test
     void save() {
-        Category category = new Category("Hi");
+        Category category = new Category("School");
         repository.save(category);
 
         Table table = new Table(dataSource, "CATEGORY");
         Assertions.assertThat(table).row(table.getRowsList().size() - 1)
-                .value("CAT_NAME").isEqualTo("Hi");
+                .value("CAT_NAME").isEqualTo("School");
 
     }
 
     @Test
     void delete() {
+        Category category = new Category("School");
+
+        Table table = new Table(dataSource, "School");
+        int expectedRows = table.getRowsList().size() - 1;
+        repository.delete(expectedRows);
+
+        table = new Table(dataSource, "School");
+
+        assertThat(table).hasNumberOfRows(expectedRows);
+        assertThat(table).row(expectedRows - 1)
+                .value("id").isNotEqualTo(category.getId());
 
     }
 
     @Test
-    void findAll() {
-
+    void findAllTest() {
+//        List<Category> categories = repository.findAll();
+//
+//        Table table = new Table(dataSource, "Category");
+//        List<Category> expected = new ArrayList<>();
+//
+//        for(Row row: table.getRowsList()){
+//            int id = Integer.parseInt(row.getColumnValue("id").getValue().toString());
+//            String name = (String) row.getColumnValue("name").getValue().toString();
+//            Category category = new Category(name);
+//            expected.add(category);
+//        }
+//
+//       assertThat(categories).containsExactly(expected.toArray(new Category[0]));
     }
 
     @Test
     void findById() {
 
-    }
+//        List<Category> categories = repository.findAll() ;
+//        categories.stream().forEach(repository::save);
+//
+//        Table categoryTable = new Table(dataSource, "Category");
+//        org.assertj.db.api.Assertions.assertThat(categoryTable).hasNumberOfRows(3);
+//
+//        Category expectedCateory = new Category("School");
+//        expectedCateory.setId(4L);
+//
+//
+//        Category actualCategory = repository.findById(4);
+//
+//        assertThat(actualCategory).isEqualTo(expectedCateory);
+//        assertThat(actualCategory.getId()).isEqualTo(expectedCateory.getId());
 
-    @Test
-    void findByName() {
     }
 }
