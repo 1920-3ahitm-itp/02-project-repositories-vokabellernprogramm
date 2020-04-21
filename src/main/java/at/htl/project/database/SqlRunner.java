1eda7271cb701bfa39d348b172ee269841aff285
htl-leonding <t.stuetz@htl-leonding.ac.at>
2020-04-21 20:50:19 +0200

    add properties-file for convenient executing of sql-scripts

--- src/main/java/at/htl/project/database/SqlRunner.java
+++ src/main/java/at/htl/project/database/SqlRunner.java
@@ -3,40 +3,43 @@ package at.htl.project.database;
 import at.htl.project.controller.Database;
 
 import javax.sql.DataSource;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.Reader;
+import java.io.*;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Properties;
 
 import org.apache.ibatis.jdbc.ScriptRunner;
 
 public class SqlRunner {
 
-  private static final List<String> sqlFilePaths =
-      List.of(
-          "sql/dr_vokabel.sql",
-          "sql/cr_vokabel.sql",
-          "sql/ins_vokabel.sql"
-      );
+//  private static final List<String> sqlFilePaths =
+//      List.of(
+//          "sql/dr_vokabel.sql",
+//          "sql/cr_vokabel.sql",
+//          "sql/ins_vokabel.sql"
+//      );
+
+  private static final String SCRIPT_PROPERTIES_PATH = "sql/script-files.properties";
 
   public static void main(String[] args) {
-    DataSource dataSource = Database.getDataSource();
-    Connection conn;
 
     try {
-      conn = dataSource.getConnection();
+      Properties scriptProperties = new Properties();
+      scriptProperties.load(new FileInputStream(SCRIPT_PROPERTIES_PATH));
+
+
+      DataSource dataSource = Database.getDataSource();
+      Connection conn = dataSource.getConnection();
       System.out.println("Connection established......");
       ScriptRunner sr = new ScriptRunner(conn);
 
-      for (String file : sqlFilePaths) {
-        Reader reader = new BufferedReader(new FileReader(file));
+      for (String file : scriptProperties.stringPropertyNames()) {
+        Reader reader = new BufferedReader(new FileReader(scriptProperties.getProperty(file)));
         sr.runScript(reader);
       }
 
-    } catch (SQLException | FileNotFoundException e) {
+    } catch (SQLException | IOException e) {
       e.printStackTrace();
     }
   }
