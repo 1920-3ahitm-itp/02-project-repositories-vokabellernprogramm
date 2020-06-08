1247758046c3bde4d134b67d4ff9a2a1944ef175
steigersdorfermarah <madschi.steigersdorfer@gmx.at>
2020-06-08 12:08:15 +0200

    added EventRepositoryTest

---
+++ src/test/java/at/htl/vocabulary/controller/EventRepositoryTest.java
@@ -0,0 +1,24 @@
+package at.htl.vocabulary.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EventRepositoryTest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+}
